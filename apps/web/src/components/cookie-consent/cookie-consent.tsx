7410df42af655403f1f8e60954558f36d1f743e8
Jake Rainis <jakerainis@gmail.com>
2021-11-16 10:03:09 -0500

    [#69] Cookie consent prompt (#70)
    * Implement cookie consent.
    
    * Fix wording.

---
+++ apps/web/src/components/cookie-consent/cookie-consent.tsx
@@ -0,0 +1,48 @@
+import Trans from 'next-translate/Trans'
+import useTranslation from 'next-translate/useTranslation'
+import { useCallback, useState } from 'react'
+
+import css from './cookie-consent.module.css'
+
+import AppLink from '@/components/app-link/app-link'
+import Button from '@/components/button'
+import { getCookie, setCookie } from '@/utils/cookies-web'
+import { urls } from '@/utils/urls'
+
+const CONSENT_COOKIE = 'cookie-consent'
+
+export default function CookieConsent() {
+  const cookie = getCookie(CONSENT_COOKIE)
+  const [hasAccepted, setHasAccepted] = useState<boolean>(!!cookie)
+  const { t } = useTranslation()
+
+  const handleAccept = useCallback(() => {
+    setCookie(CONSENT_COOKIE, '1', 365)
+    setHasAccepted(true)
+  }, [])
+
+  if (hasAccepted) return null
+
+  return (
+    <div className={css.root}>
+      <div className={css.wrapper}>
+        <div>
+          <Trans
+            components={[
+              <p key={0} />,
+              <p key={1} />,
+              <AppLink key={2} href={urls.termsAndConditions} />,
+              <AppLink key={3} href={urls.privacyPolicy} />,
+            ]}
+            i18nKey="common:global.consent"
+          />
+        </div>
+        <div className={css.acceptButton}>
+          <Button onClick={handleAccept} size="small" variant="primary">
+            {t('common:actions.Accept')}
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
