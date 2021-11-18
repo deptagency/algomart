a2ac4cb9b2322dcd8e3bd6958fab41164c3fc766
Jake Rainis <jakerainis@gmail.com>
2021-11-18 16:53:33 -0500

    [MISC] Pack Opening improvements & TS Node Fix (#91)
    * Allow multiple grid styles
    
    * Pack opening and tsnode fix.

--- apps/web/src/components/pack-grid/pack-placeholder.tsx
+++ apps/web/src/components/pack-grid/pack-placeholder.tsx
@@ -3,34 +3,44 @@ import useTranslation from 'next-translate/useTranslation'
 
 import css from './pack-placeholder.module.css'
 export interface PackPlaceholderProps {
-  index: number
   collectibleEdition: string
+  hideContent?: boolean
+  index: number
   packItemNumber: number
   packTitle: string
   packTotalCount: number
 }
 export default function PackPlaceholder({
-  index,
+  hideContent,
   collectibleEdition,
-  packTitle,
+  index,
   packItemNumber,
+  packTitle,
   packTotalCount,
 }: PackPlaceholderProps) {
   const { t } = useTranslation()
   return (
     <div
-      className={clsx(css.root, css[`backgroundPosition-${(index + 1) % 6}`])}
+      className={clsx(css.root, css[`backgroundPosition-${(index + 1) % 6}`], {
+        [css.hideContent]: hideContent,
+      })}
     >
       <div className={css.questionMark}>?</div>
-      <div className={css.packName}>{packTitle}</div>
-      <div className={css.infoWrapper}>
-        <div className={clsx(css.infoWrapperColumn, css.infoWrapperLeft)}>
-          {collectibleEdition}
-        </div>
-        <div className={clsx(css.infoWrapperColumn, css.infoWrapperRight)}>
-          {`${packItemNumber} ${t('collection:viewer.of')} ${packTotalCount}`}
-        </div>
-      </div>
+      {!hideContent && (
+        <>
+          <div className={css.packName}>{packTitle}</div>
+          <div className={css.infoWrapper}>
+            <div className={clsx(css.infoWrapperColumn, css.infoWrapperLeft)}>
+              {collectibleEdition}
+            </div>
+            <div className={clsx(css.infoWrapperColumn, css.infoWrapperRight)}>
+              {`${packItemNumber} ${t(
+                'collection:viewer.of'
+              )} ${packTotalCount}`}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   )
 }
