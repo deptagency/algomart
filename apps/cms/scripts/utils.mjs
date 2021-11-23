c377cb5bbf4443f56675efacb885ce2f7d42f34c
Adam Fraser <adam.n.fraser@gmail.com>
2021-11-23 12:32:51 -0500

    one more

--- apps/cms/scripts/utils.mjs
+++ apps/cms/scripts/utils.mjs
@@ -207,8 +207,3 @@ export function readlineAsync(prompt) {
     })
   })
 }
-
-export const isUuid = (uuid) => {
-  const regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
-  return regex.test(uuid)
-}
