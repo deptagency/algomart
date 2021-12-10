5b233beed76b12ac614dca5f47005dd517b61598
Adam Fraser <adam.n.fraser@gmail.com>
2021-12-10 13:16:49 -0500

    render brand logos

--- apps/web/src/pages/brands/index.tsx
+++ apps/web/src/pages/brands/index.tsx
@@ -1,8 +1,13 @@
 import { Brands } from '@algomart/schemas'
 import { GetServerSidePropsContext } from 'next'
+import Image from 'next/image'
+
+import css from './brands.module.css'
 
 import { ApiClient } from '@/clients/api-client'
+import Heading from '@/components/heading'
 import DefaultLayout from '@/layouts/default-layout'
+import { cmsImageLoader } from '@/utils/cms-image-loader'
 
 interface BrandsProps {
   brands: Brands
@@ -12,10 +17,26 @@ interface BrandsProps {
 export default function BrandsPage({ total, brands }: BrandsProps) {
   return (
     <DefaultLayout noPanel>
-      Brands: {total}
-      <ul>
+      <ul className={css.brandsContainer}>
         {brands.map((brand: any) => (
-          <li key={brand.id}>{brand.name}</li>
+          <li key={brand.id} className={css.brandCard}>
+            {!brand.logo && (
+              <Heading level={3} className={css.brandNoLogo}>
+                {brand.name}
+              </Heading>
+            )}
+            {brand.logo && (
+              <Image
+                loader={cmsImageLoader}
+                src={brand.logo}
+                alt={brand.name}
+                width={356}
+                height={356}
+                objectFit="cover"
+                layout="responsive"
+              />
+            )}
+          </li>
         ))}
       </ul>
     </DefaultLayout>
