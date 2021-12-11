1d78663b6935e70c58fe0df3becb4d2845b7654c
Sean Winner <srwinner16@gmail.com>
2021-12-11 12:33:30 -0800

    try this

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -247,3 +247,28 @@ variable "web_node_env" {
 
 variable "web_revision_name" {
 }
+
+output "cms_domain_mapping" {
+  value = var.cms_domain_mapping
+}
+output "api_domain_mapping" {
+  value = var.api_domain_mapping
+}
+output "web_domain_mapping" {
+  value = var.web_domain_mapping
+}
+output "credentials" {
+  value = var.credentials
+}
+output "api_image" {
+  value = var.api_image
+}
+output "api_service_name" {
+  value = var.api_service_name
+}
+output "region" {
+  value = var.region
+}
+output "api_revision_name" {
+  value = var.api_revision_name
+}
