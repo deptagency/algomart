65f29af00262486f47970e08eadf8b83e04ffd96
james-jin-0903 <jinzhengyou0903@gmail.com>
2021-12-28 11:00:51 -0600

    Customize collections page

--- apps/web/src/components/collectibles/no-collectibles-content.tsx
+++ apps/web/src/components/collectibles/no-collectibles-content.tsx
@@ -1,9 +1,9 @@
 import useTranslation from 'next-translate/useTranslation'
 
-import css from './no-collectibles-content.module.css'
+// import css from './no-collectibles-content.module.css'
 
 import Button from '@/components/button'
-import CollectiblePlaceholder from '@/components/collectibles/collectible-placeholder'
+// import CollectiblePlaceholder from '@/components/collectibles/collectible-placeholder'
 import Heading from '@/components/heading'
 
 export interface NoCollectiblesContentProps {
@@ -15,20 +15,14 @@ export default function NoCollectiblesContent({
 }: NoCollectiblesContentProps) {
   const { t } = useTranslation()
   return (
-    <div className={css.root}>
-      <div className={css.gridContainer}>
-        {Array.from({ length: 3 })
-          .fill('x')
-          .map((_, index) => (
-            <CollectiblePlaceholder key={String(index)} />
-          ))}
-      </div>
-
-      <Heading className={css.heading} level={3}>
+    <div className='flex flex-col items-center'>
+      <Heading className='text-gray-200 text-3xl mb-8 font-bold my-12' level={3}>
+        No collectibles
+      </Heading>
+      <Heading className='text-blue-800 font-bold my-12' level={3}>
         {t('collection:viewer.startCollection')}
       </Heading>
-
-      <Button onClick={handleRedirect}>
+      <Button onClick={handleRedirect} className='mx-20 w-80 mb-12'>
         {t('collection:viewer.Find Something Cool')}
       </Button>
     </div>
