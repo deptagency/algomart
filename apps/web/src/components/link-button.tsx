c46eb718eaf6dc8e72cd9eb30b2ef6ea0d72d902
Simon Ingeson <44818+smonn@users.noreply.github.com>
2022-01-25 14:50:15 -0500

    setup link-button component

---
+++ apps/web/src/components/link-button.tsx
@@ -0,0 +1,61 @@
+import Link, { LinkProps } from 'next/link'
+import { AnchorHTMLAttributes, DetailedHTMLProps } from 'react'
+
+import { ButtonBaseProps, buttonClasses } from './button'
+
+export type LinkButtonProps = LinkProps & {
+  disabled?: boolean
+} & ButtonBaseProps &
+  Omit<
+    DetailedHTMLProps<
+      AnchorHTMLAttributes<HTMLAnchorElement>,
+      HTMLAnchorElement
+    >,
+    'href'
+  >
+
+export default function LinkButton({
+  children,
+  className,
+  href,
+  as,
+  replace,
+  scroll,
+  disabled,
+  shallow,
+  passHref,
+  prefetch,
+  locale,
+  disablePadding,
+  size,
+  variant,
+  fullWidth,
+  ...rest
+}: LinkButtonProps) {
+  return (
+    <Link
+      href={href}
+      as={as}
+      replace={replace}
+      scroll={scroll}
+      shallow={shallow}
+      passHref={passHref}
+      prefetch={prefetch}
+      locale={locale}
+    >
+      <a
+        className={buttonClasses({
+          className,
+          disablePadding,
+          size,
+          fullWidth,
+          disabled,
+          variant,
+        })}
+        {...rest}
+      >
+        {children}
+      </a>
+    </Link>
+  )
+}
