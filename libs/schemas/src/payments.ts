a04f84497a6b123e838c294852594738bbfe6820
Dallas Huggins <dallas.a.huggins@gmail.com>
2022-01-28 09:53:17 -0500

    Cleanup

--- libs/schemas/src/payments.ts
+++ libs/schemas/src/payments.ts
@@ -622,7 +622,7 @@ export const GetPaymentCardStatusSchema = Type.Object({
 export const PaymentSchema = Type.Intersect([
   BaseSchema,
   PaymentBaseSchema,
-  Type.Omit(ToPaymentBaseSchema, ['externalId', 'amount', 'sourceId', 'error']),
+  Type.Omit(ToPaymentBaseSchema, ['externalId', 'amount', 'sourceId']),
   Type.Object({
     externalId: Nullable(Type.String({ format: 'uuid' })),
   }),
