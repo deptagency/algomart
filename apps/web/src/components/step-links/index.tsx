e41428978eb3ce723e8e13e5a58cfed28a3f3b2d
Adam Fraser <adam.n.fraser@gmail.com>
2022-02-01 13:00:52 -0500

    Rename Breadcrumbs to StepLinks (breadcrumbs refer to a page and it's ancestors and this component can't be reused for this purpose)

--- apps/web/src/components/step-links/index.tsx
+++ apps/web/src/components/step-links/index.tsx
@@ -3,30 +3,31 @@ import clsx from 'clsx'
 import { useRouter } from 'next/router'
 import React from 'react'
 
-import css from './breadcrumbs.module.css'
+import css from './step-links.module.css'
 
 import AppLink from '@/components/app-link/app-link'
 import { isMatchingQueryParams } from '@/utils/urls'
 
-export interface BreadcrumbsProps {
-  breadcrumbs: {
+export interface StepLinksProps {
+  links: {
     label: string
     href: string
     handleClick?: () => void
   }[]
 }
 
-export default function Breadcrumbs({ breadcrumbs }: BreadcrumbsProps) {
+/** */
+export default function StepLinks({ links }: StepLinksProps) {
   const { asPath } = useRouter()
-  if (!breadcrumbs) return null
+  if (!links) return null
   return (
-    <nav className={css.navigation}>
-      <ol className={css.breadcrumbs}>
-        {breadcrumbs.map(({ href, label, handleClick }, index) => {
+    <nav>
+      <ol className={css.stepLinks}>
+        {links.map(({ href, label, handleClick }, index) => {
           const isCurrentNavItem = isMatchingQueryParams(asPath, href)
           return (
             <React.Fragment key={index}>
-              <li className={css.breadcrumb}>
+              <li className={css.stepLink}>
                 <AppLink
                   className={clsx(css.navLink, {
                     [css.navLinkActive]: isCurrentNavItem,
@@ -37,7 +38,7 @@ export default function Breadcrumbs({ breadcrumbs }: BreadcrumbsProps) {
                   {label}
                 </AppLink>
               </li>
-              {index < breadcrumbs.length - 1 && (
+              {index < links.length - 1 && (
                 <ChevronRightIcon className={css.icon} />
               )}
             </React.Fragment>
