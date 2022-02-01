c3310ac90516fb1e66dfb82269e671409daa8901
tj-boyle <thomas.john.boyle@gmail.com>
2022-02-01 12:51:27 -0500

    Readme typos, clarity. Better directus executor failing (#264)
    * updating readmes to be more clear
    
    * update directus executor to check exit code
    
    * remove console logs :)
    
    * readd success :) :)
    
    * Apply suggestions from code review
    
    return from reject
    
    Co-authored-by: Simon Ingeson <44818+smonn@users.noreply.github.com>
    
    * removing outdated portion of cms readme, updating data model import/export information
    
    Co-authored-by: Simon Ingeson <44818+smonn@users.noreply.github.com>

--- tools/executors/directus/impl.ts
+++ tools/executors/directus/impl.ts
@@ -45,7 +45,12 @@ async function runAction(
       reject(error)
     })
 
-    directus.on('exit', () => {
+    directus.on('exit', (code) => {
+      if (code !== 0) {
+        reject(code)
+        return
+      }
+
       resolve(true)
     })
   })
