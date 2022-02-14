a4c2510595a1a97d5438d6aa11265410fe947f79
Dallas Huggins <dallas.a.huggins@gmail.com>
2022-02-14 10:05:15 -0500

    Checking payment on failure/success pages

--- apps/web/src/pages/checkout/failure.tsx
+++ apps/web/src/pages/checkout/failure.tsx
@@ -1,52 +1,60 @@
-import {
-  PackAuction,
-  PackStatus,
-  PackType,
-  PublishedPack,
-} from '@algomart/schemas'
+import { Payment } from '@algomart/schemas'
 import { GetServerSideProps } from 'next'
 import useTranslation from 'next-translate/useTranslation'
-import { useEffect } from 'react'
 
 import { ApiClient } from '@/clients/api-client'
-import { Analytics } from '@/clients/firebase-analytics'
-import { usePaymentProvider } from '@/contexts/payment-context'
-import { Environment } from '@/environment'
 import DefaultLayout from '@/layouts/default-layout'
 import {
   getAuthenticatedUser,
   handleUnauthenticatedRedirect,
 } from '@/services/api/auth-service'
-import { urls } from '@/utils/urls'
 
 export interface FailurePageProps {
-  auctionPackId: string | null
-  currentBid: number | null
-  release: PublishedPack
+  payment: Payment
 }
 
-export default function FailurePage({
-  auctionPackId,
-  currentBid,
-  release,
-}: FailurePageProps) {
+export default function FailurePage({ payment }: FailurePageProps) {
+  console.log('Failure payment:', payment)
   const { t } = useTranslation()
-  // const paymentProps = usePaymentProvider({
-  //   auctionPackId,
-  //   currentBid,
-  //   release,
-  // })
   return <DefaultLayout pageTitle="Failure!" panelPadding></DefaultLayout>
 }
 
 export const getServerSideProps: GetServerSideProps<FailurePageProps> = async (
   context
 ) => {
+  const { paymentId } = context.query
+
+  // Payment ID is required
+  if (!paymentId || typeof paymentId !== 'string') {
+    return {
+      notFound: true,
+    }
+  }
+
   // Verify authentication
   const user = await getAuthenticatedUser(context)
   if (!user) {
     return handleUnauthenticatedRedirect(context.resolvedUrl)
   }
 
-  // @TODO: Get payment
+  // Get payment
+  const payment = await ApiClient.instance.getPaymentById(paymentId)
+
+  // Check if payment is found
+  if (!payment) {
+    return {
+      notFound: true,
+    }
+  }
+
+  // Confirm logged-in user is owner of pack
+  if (payment.payerId !== user.id) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { payment },
+  }
 }
--- apps/web/src/pages/checkout/success.tsx
+++ apps/web/src/pages/checkout/success.tsx
@@ -1,52 +1,60 @@
-import {
-  PackAuction,
-  PackStatus,
-  PackType,
-  PublishedPack,
-} from '@algomart/schemas'
+import { Payment } from '@algomart/schemas'
 import { GetServerSideProps } from 'next'
 import useTranslation from 'next-translate/useTranslation'
-import { useEffect } from 'react'
 
 import { ApiClient } from '@/clients/api-client'
-import { Analytics } from '@/clients/firebase-analytics'
-import { usePaymentProvider } from '@/contexts/payment-context'
-import { Environment } from '@/environment'
 import DefaultLayout from '@/layouts/default-layout'
 import {
   getAuthenticatedUser,
   handleUnauthenticatedRedirect,
 } from '@/services/api/auth-service'
-import { urls } from '@/utils/urls'
 
 export interface SuccessPageProps {
-  auctionPackId: string | null
-  currentBid: number | null
-  release: PublishedPack
+  payment: Payment
 }
 
-export default function SuccessPage({
-  auctionPackId,
-  currentBid,
-  release,
-}: SuccessPageProps) {
+export default function SuccessPage({ payment }: SuccessPageProps) {
+  console.log('Success payment:', payment)
   const { t } = useTranslation()
-  // const paymentProps = usePaymentProvider({
-  //   auctionPackId,
-  //   currentBid,
-  //   release,
-  // })
   return <DefaultLayout pageTitle="Success!" panelPadding></DefaultLayout>
 }
 
 export const getServerSideProps: GetServerSideProps<SuccessPageProps> = async (
   context
 ) => {
+  const { paymentId } = context.query
+
+  // Payment ID is required
+  if (!paymentId || typeof paymentId !== 'string') {
+    return {
+      notFound: true,
+    }
+  }
+
   // Verify authentication
   const user = await getAuthenticatedUser(context)
   if (!user) {
     return handleUnauthenticatedRedirect(context.resolvedUrl)
   }
 
-  // @TODO: Get payment
+  // Get payment
+  const payment = await ApiClient.instance.getPaymentById(paymentId)
+
+  // Check if payment is found
+  if (!payment) {
+    return {
+      notFound: true,
+    }
+  }
+
+  // Confirm logged-in user is owner of pack
+  if (payment.payerId !== user.id) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { payment },
+  }
 }
