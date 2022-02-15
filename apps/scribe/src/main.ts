fd813f3d0304ff583ed2ea263d591c70a954195b
Alan Jones <alanjones@rocketinsights.com>
2022-02-15 10:42:52 -0500

    scribe: Setup a basic toad scheduler task

--- apps/scribe/src/main.ts
+++ apps/scribe/src/main.ts
@@ -1,1 +1,13 @@
-console.log('Hello World!')
+import { ToadScheduler, SimpleIntervalJob, Task } from 'toad-scheduler'
+
+const scheduler = new ToadScheduler()
+
+const task = new Task('simple task', () => {
+  console.log('hello world')
+})
+const job = new SimpleIntervalJob({ seconds: 5 }, task)
+
+scheduler.addSimpleIntervalJob(job)
+
+// ...when stopping your app
+//scheduler.stop()
