d17f9702375d66ca5bd9cf0c6cf9d4a8cab82f87
Donald Chan <buckboy@Donalds-MacBook-Pro.local>
2022-02-15 21:23:04 -0600

    Remove migration steps from scribe

--- docker/deploy/scribe/run.sh
+++ docker/deploy/scribe/run.sh
@@ -4,15 +4,6 @@ set -e
 
 cd /app
 
-echo 'current version, before:'
-nx run scribe:migrate:currentVersion
-
-echo 'applying migrations...'
-nx run scribe:migrate:latest
-
-echo 'current version, after:'
-nx run scribe:migrate:currentVersion
-
 echo 'starting scribe...'
 cd /app/dist/apps/scribe
 node main.js
