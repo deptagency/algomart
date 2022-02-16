bbbacc8d2e2201fae47825437ca2e85950a1737a
TJ Boyle <thomas.john.boyle@gmail.com>
2022-02-16 13:41:44 -0500

    task now uses i18n service

--- apps/api/src/tasks/update-currency-conversions.task.ts
+++ apps/api/src/tasks/update-currency-conversions.task.ts
@@ -2,28 +2,33 @@ import { DEFAULT_CURRENCY } from '@algomart/schemas'
 import { Model } from 'objection'
 
 import { Configuration } from '@/configuration'
-import CoinbaseAdapter from '@/lib/coinbase-adapter'
-import CollectiblesService from '@/modules/collectibles/collectibles.service'
+import I18nService from '@/modules/i18n/i18n.service'
 import DependencyResolver from '@/shared/dependency-resolver'
 import { logger } from '@/utils/logger'
 
-// TODO: RETHINK
 export default async function updateCurrencyConversions(
   registry: DependencyResolver
 ) {
   const log = logger.child({ task: 'update-currency-conversions' })
-  const coinbase = new CoinbaseAdapter({
-    url: Configuration.coinbaseUrl,
-  })
+  const sourceCurrency = Configuration.currency?.code || DEFAULT_CURRENCY
+  const i18nService = registry.get<I18nService>(I18nService.name)
   const trx = await Model.startTransaction()
   try {
-    const result = await coinbase.getExchangeRates({
-      currency: DEFAULT_CURRENCY,
-    })
-    log.info('stored %d collectibles on IPFS', result)
+    const result = await i18nService.getCurrencyConversions(
+      {
+        sourceCurrency,
+      },
+      trx
+    )
+    log.info(
+      `stored ${
+        Object.keys(result).length
+      } currency conversions for ${sourceCurrency} in db`,
+      result
+    )
     await trx.commit()
   } catch (error) {
     await trx.rollback()
-    log.error(error as Error, 'failed to store collectibles')
+    log.error(error as Error, 'failed to store currency conversions')
   }
 }
