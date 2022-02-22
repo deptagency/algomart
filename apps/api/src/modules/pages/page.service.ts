b2bad544497696d83dad924f4c0a8c7da7b130a5
Jon Principe <jonathan.principe@hey.com>
2022-02-22 10:53:44 -0500

    merge updates

--- apps/api/src/modules/pages/page.service.ts
+++ apps/api/src/modules/pages/page.service.ts
@@ -1,14 +1,14 @@
-import { DEFAULT_LOCALE, Page } from '@algomart/schemas'
+import { DEFAULT_LOCALE, DirectusPageTranslation } from '@algomart/schemas'
 import { CMSCacheAdapter } from '@algomart/shared/adapters'
 import { userInvariant } from '@algomart/shared/utils'
 
 export default class DirectusPageService {
-  constructor(private readonly cms: CMSCacheAdapter) { }
+  constructor(private readonly cms: CMSCacheAdapter) {}
 
   async getPage(
     slug,
     locale = DEFAULT_LOCALE
-  ): Promise<PageTranslation> {
+  ): Promise<DirectusPageTranslation> {
     const page = await this.cms.getPage(slug, locale)
     userInvariant(page, 'page not found', 404)
 
