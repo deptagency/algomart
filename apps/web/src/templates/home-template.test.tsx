c29382fd4c6f1479ac6090b6a40161c04747025c
Simon Ingeson <44818+smonn@users.noreply.github.com>
2022-03-15 13:48:42 -0400

    fix broken tests

--- apps/web/src/templates/home-template.test.tsx
+++ apps/web/src/templates/home-template.test.tsx
@@ -3,6 +3,7 @@ import {
   PackCollectibleOrder,
   PackStatus,
   PackType,
+  PublishedPack,
 } from '@algomart/schemas'
 import { render, screen } from '@testing-library/react'
 
@@ -10,33 +11,36 @@ import HomeTemplate from './home-template'
 
 describe('HomeTemplate', () => {
   test('should render pack title', async () => {
+    const featuredPack: PublishedPack = {
+      activeBid: undefined,
+      allowBidExpiration: false,
+      auctionUntil: undefined,
+      available: 10,
+      body: undefined,
+      collectibleTemplateIds: [],
+      config: {
+        collectibleDistribution: PackCollectibleDistribution.Random,
+        collectibleOrder: PackCollectibleOrder.Random,
+        collectiblesPerPack: 1,
+      },
+      image: 'http://localhost/image.png',
+      onePackPerCustomer: false,
+      price: 0,
+      releasedAt: undefined,
+      slug: '',
+      status: PackStatus.Active,
+      subtitle: undefined,
+      templateId: '',
+      title: 'My Featured Pack',
+      total: 100,
+      type: PackType.Free,
+      additionalImages: [],
+    }
+
     render(
       <HomeTemplate
         onClickFeatured={jest.fn()}
-        featuredPack={{
-          activeBid: undefined,
-          allowBidExpiration: false,
-          auctionUntil: undefined,
-          available: 10,
-          body: undefined,
-          collectibleTemplateIds: [],
-          config: {
-            collectibleDistribution: PackCollectibleDistribution.Random,
-            collectibleOrder: PackCollectibleOrder.Random,
-            collectiblesPerPack: 1,
-          },
-          image: 'http://localhost/image.png',
-          onePackPerCustomer: false,
-          price: 0,
-          releasedAt: undefined,
-          slug: '',
-          status: PackStatus.Active,
-          subtitle: undefined,
-          templateId: '',
-          title: 'My Featured Pack',
-          total: 100,
-          type: PackType.Free,
-        }}
+        featuredPack={featuredPack}
         upcomingPacks={[]}
         notableCollectibles={[]}
       />
