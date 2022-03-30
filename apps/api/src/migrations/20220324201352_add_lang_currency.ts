4251be95de1b509b95e95a1f145cc7dcc6f01425
TJ Boyle <thomas.john.boyle@gmail.com>
2022-03-30 17:26:53 -0400

    add default to for sort

--- apps/api/src/migrations/20220324201352_add_lang_currency.ts
+++ apps/api/src/migrations/20220324201352_add_lang_currency.ts
@@ -9,7 +9,7 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('CmsCacheLanguages', (table) => {
     table.string('code').primary()
     table.string('name').notNullable()
-    table.string('sort').unique().notNullable()
+    table.string('sort').unique().notNullable().defaultTo(1)
     table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable()
     table.timestamp('updatedAt').defaultTo(knex.fn.now()).notNullable()
   })
