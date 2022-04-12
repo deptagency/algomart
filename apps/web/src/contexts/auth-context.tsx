0b711e42ac20a4a5ec7c4f61eb4056b29169e3e3
TJ Boyle <thomas.john.boyle@gmail.com>
2022-04-12 14:52:16 -0400

    fixes anonymous preference changes not updating profile once logged in

--- apps/web/src/contexts/auth-context.tsx
+++ apps/web/src/contexts/auth-context.tsx
@@ -1,3 +1,4 @@
+import { CURRENCY_COOKIE, LANG_COOKIE } from '@algomart/schemas'
 import {
   Auth,
   createUserWithEmailAndPassword,
@@ -34,7 +35,7 @@ import {
   SignUpPayload,
 } from '@/types/auth'
 import { FileWithPreview } from '@/types/file'
-import { removeCookie, setCookie } from '@/utils/cookies-web'
+import { getCookie, removeCookie, setCookie } from '@/utils/cookies-web'
 import {
   ActionsUnion,
   createAction,
@@ -146,6 +147,48 @@ export function useAuthProvider() {
       if (profileResponse.email !== auth.currentUser.email) {
         await fetch(urls.api.v1.updateEmail, {
           body: JSON.stringify({ email: auth.currentUser.email }),
+          headers: {
+            authorization: `bearer ${token}`,
+            'content-type': 'application/json',
+          },
+          method: 'PUT',
+        })
+      }
+
+      /**
+       * If an anonymous user changes their preferred language, then logs in,
+       * we need to update the db to reflect this preference
+       */
+      const languageCookie = getCookie(LANG_COOKIE)
+      const parsedLanguageCookie =
+        languageCookie && languageCookie !== 'null' ? languageCookie : null
+      if (
+        parsedLanguageCookie &&
+        profileResponse.language !== parsedLanguageCookie
+      ) {
+        await fetch(urls.api.v1.updateLanguage, {
+          body: JSON.stringify({ parsedLanguageCookie }),
+          headers: {
+            authorization: `bearer ${token}`,
+            'content-type': 'application/json',
+          },
+          method: 'PUT',
+        })
+      }
+
+      /**
+       * If an anonymous user changes their preferred currency, then logs in,
+       * we need to update the db to reflect this preference
+       */
+      const currencyCookie = getCookie(CURRENCY_COOKIE)
+      const parsedCurrencyCookie =
+        currencyCookie && currencyCookie !== 'null' ? currencyCookie : null
+      if (
+        parsedCurrencyCookie &&
+        profileResponse.currency !== parsedCurrencyCookie
+      ) {
+        await fetch(urls.api.v1.updateCurrency, {
+          body: JSON.stringify({ parsedCurrencyCookie }),
           headers: {
             authorization: `bearer ${token}`,
             'content-type': 'application/json',
