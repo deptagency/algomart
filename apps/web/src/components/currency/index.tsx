26729b13aa489ebd2af4b952db8eedff8450d421
Adam Fraser <adam.n.fraser@gmail.com>
2022-04-29 13:29:57 -0400

    fixes hydration on home page

--- apps/web/src/components/currency/index.tsx
+++ apps/web/src/components/currency/index.tsx
@@ -1,3 +1,6 @@
+import { DEFAULT_CURRENCY, DEFAULT_LOCALE } from '@algomart/schemas'
+import { useEffect, useState } from 'react'
+
 import { useCurrency } from '@/contexts/currency-context'
 import { useI18n } from '@/contexts/i18n-context'
 import { useLocale } from '@/hooks/use-locale'
@@ -24,6 +27,20 @@ export default function Currency({
   const globalLocale = useLocale()
   const { currency: globalCurrency } = useCurrency()
   const { conversionRate } = useI18n()
+  const [hydrated, setHydrated] = useState(false)
+
+  if (!hydrated) {
+    currency = DEFAULT_CURRENCY
+    locale = DEFAULT_LOCALE
+  }
+
+  useEffect(() => {
+    /*
+     * This addresses an issue where the user's selected currency is not
+     * available at hydration time, causing a discrepancy between server and client
+     */
+    setHydrated(true)
+  }, [])
 
   const currencyCode = currency || globalCurrency
   const localeCode = locale || globalLocale
